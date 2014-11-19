a56c894873b728775ea7389dd2c396a1718e03ac
Kemado <jefferson-l-bernardo@hotmail.com>
2014-11-19 10:34:30 -0200

    Teste campo de senha

--- pessoas/models.py
+++ pessoas/models.py
@@ -4,5 +4,5 @@ class Pessoa(models.Model):
   nome = models.CharField(max_length=100)
   email = models.EmailField()
   usuario = models.CharField(max_length=20)
-  senha = models.CharField(max_length=15)
+  senha = models.CharField(widget=forms.PasswordInput)
   
